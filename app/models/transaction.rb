45aa50a5c21a9e02a6346e36c6e7e84ba9c51ac1
zq5q37 <zq5q37@gmail.com>
2024-06-19 21:46:56 +0800

    updated till carousel

---
+++ app/models/transaction.rb
@@ -0,0 +1,13 @@
+class Transaction < ApplicationRecord
+    validates :name, presence: true
+    validates :amount, numericality: { greater_than: 0 }
+    validate :amount_must_have_two_decimal_places
+
+  private
+
+  def amount_must_have_two_decimal_places
+    unless amount.to_s.match?(/\A\d+(\.\d{1,2})?\z/)
+      errors.add(:amount, "must have at most two decimal places")
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+=======
+  resources :transactions
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "transactions#index"
+>>>>>>> Stashed changes
 end
