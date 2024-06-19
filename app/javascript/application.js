ac4008f0a2fe192de4f7d1b448d5d05b1f5b85d8
Nicole Cheah <nicolecheah08@gmail.com>
2024-06-19 17:13:47 +0800

    Popup with autofill function

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,25 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    var setupButton = document.getElementById('setupButton');
+    var popup = document.getElementById('popup');
+    var closePopup = document.getElementById('closePopup');
+  
+    setupButton.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevent the default behavior of the link
+  
+      popup.style.display = "block";
+    });
+  
+    closePopup.onclick = function() {
+      popup.style.display = "none";
+    }
+  
+    window.onclick = function(event) {
+      if (event.target == popup) {
+        popup.style.display = "none";
+      }
+    }
+  });
