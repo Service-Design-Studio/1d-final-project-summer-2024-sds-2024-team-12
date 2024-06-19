91ca0d6aca735c69be0dc304bee180ae5552133d
DawdlingFish865 <66412317+DawdlingFish865@users.noreply.github.com>
2024-06-19 17:41:28 +0800

    Update routes.rb
    Signed-off-by: DawdlingFish865 <66412317+DawdlingFish865@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'login', to: 'sessions#new'
   resources :transactions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root "transactions#index"
 end
