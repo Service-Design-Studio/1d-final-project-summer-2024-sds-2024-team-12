4c5c613b79c736cd9b665e9e0e214d75145bc23d
Jeslyn Wang <jeslynwjx@gmail.com>
2024-07-04 01:43:26 +0800

    cardlimit

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
-
   get 'scheduled_transactions/new'
   post 'scheduled_transactions/create'
   get 'pay_and_transfer_page/index'
   get 'paynow/index'
-  resources :shortcut_buttons, only: [:new, :create, :destroy ]
-  resources :scheduled_transactions, only: [:new, :create,]
+  get 'transactions/index'
+
+  resources :shortcut_buttons, only: [:new, :create, :destroy]
+  resources :scheduled_transactions, only: [:new, :create]
   resources :transactions, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
+  resources :cardlimit, only: [:new, :paynow] 
 
+    
+  get '/cardlimit/paynow', to: 'cardlimit#paynow', as: 'cardlimit_paynow'
+  get '/cardlimit/adjusting', to: 'cardlimit#adjusting', as: 'cardlimit_adjusting'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "transactions#index"
 end
-
-
