00ac55400d6903d7f26e97655c5e1c273a3637d1
Jeslyn Wang <jeslynwjx@gmail.com>
2024-07-11 21:44:03 +0800

    fixed deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
+  config.hosts.clear
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.secret_key_base = "KcqewtouDaXPAznESRZGgfrNysxVbLHilFkWjTmMChJdQIOBvp"
+  
 end
