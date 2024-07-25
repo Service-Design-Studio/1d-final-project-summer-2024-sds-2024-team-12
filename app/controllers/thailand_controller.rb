27cb141ace62095b3258251311559c8585d46b76
zizipurple <tanzihui823@gmail.com>
2024-07-25 12:24:31 +0800

    commented out useless controllers

--- app/controllers/thailand_controller.rb
+++ app/controllers/thailand_controller.rb
@@ -5,27 +5,27 @@ class ThailandController < ApplicationController
       @promptpay = Promptpay.new
   end
 
-  # GET /promptpays or /promptpays.json
-  def index
-    @promptpays = Promptpay.all
-  end
+  # # GET /promptpays or /promptpays.json
+  # def index
+  #   @promptpays = Promptpay.all
+  # end
 
-  def history
-    @promptpays = Promptpay.all
-  end
+  # def history
+  #   @promptpays = Promptpay.all
+  # end
 
-  # GET /promptpays/1 or /promptpays/1.json
-  def show
-  end
+  # # GET /promptpays/1 or /promptpays/1.json
+  # def show
+  # end
 
-  # GET /promptpays/new
-  def new
-    @promptpay = Promptpay.new
-  end
+  # # GET /promptpays/new
+  # def new
+  #   @promptpay = Promptpay.new
+  # end
 
-  # GET /promptpays/1/edit
-  def edit
-  end
+  # # GET /promptpays/1/edit
+  # def edit
+  # end
 
   # POST /promptpays or /promptpays.json
   def create
@@ -44,28 +44,28 @@ class ThailandController < ApplicationController
   
   
 
-  # PATCH/PUT /promptpays/1 or /promptpays/1.json
-  def update
-    respond_to do |format|
-      if @promptpay.update(promptpay_params)
-        format.html { redirect_to promptpay_url(@promptpay), notice: "Promptpay transaction was successfully updated." }
-        format.json { render :show, status: :ok, location: @promptpay }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @promptpay.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # # PATCH/PUT /promptpays/1 or /promptpays/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @promptpay.update(promptpay_params)
+  #       format.html { redirect_to promptpay_url(@promptpay), notice: "Promptpay transaction was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @promptpay }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @promptpay.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
-  # DELETE /promptpays/1 or /promptpays/1.json
-  def destroy
-    @promptpay.destroy
+  # # DELETE /promptpays/1 or /promptpays/1.json
+  # def destroy
+  #   @promptpay.destroy
 
-    respond_to do |format|
-      format.html { redirect_to promptpays_url, notice: "Promptpay transaction was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  #   respond_to do |format|
+  #     format.html { redirect_to promptpays_url, notice: "Promptpay transaction was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
