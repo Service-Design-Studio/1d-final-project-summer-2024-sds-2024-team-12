54e4f40a5c3b6b006e39ff14436a176fc5979f76
zq5q37 <zq5q37@gmail.com>
2024-07-29 19:19:57 +0800

    change to turbo:load

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -41,67 +41,6 @@ document.addEventListener('turbo:load', (event) => {
   });
 });
 
-// document.addEventListener('DOMContentLoaded', (event) => {
-//   var stopShowingButtons = document.querySelectorAll('.stopShowingButton');
-
-//   stopShowingButtons.forEach((stopShowingButton, index) => {
-//     var carouselItem = document.getElementById(`carouselItem${index}`);
-//     var carouselIndicators = document.getElementById('carouselIndicators');
-
-//     stopShowingButton.addEventListener('click', function() {
-//       // Remove the carousel item
-//       carouselItem.remove();
-//       // Remove the corresponding indicator
-//       var indicatorToRemove = carouselIndicators.querySelector(`[data-slide-to="${index}"]`);
-//       if (indicatorToRemove) {
-//         indicatorToRemove.remove();
-//       }
-//       // Update the active indicator if needed
-//       var activeIndicator = carouselIndicators.querySelector('.active');
-//       if (activeIndicator) {
-//         activeIndicator.classList.remove('active');
-//         if (index > 0) {
-//           carouselIndicators.children[index - 1].classList.add('active');
-//         } else {
-//           carouselIndicators.firstChild.classList.add('active');
-//         }
-//       }
-//       // Optionally, add logic to remember the user preference (e.g., using localStorage, cookies, or sending a request to the server)
-//       localStorage.setItem(`hideCarouselItem${index}`, 'true');
-//     });
-
-//     if (localStorage.getItem(`hideCarouselItem${index}`) === 'true') {
-//       carouselItem.style.display = 'none';
-//       var indicatorToRemove = carouselIndicators.querySelector(`[data-slide-to="${index}"]`);
-//       if (indicatorToRemove) {
-//         indicatorToRemove.style.display = 'none';
-//       }
-//     }
-//   });
-// });
-
-
-
-
-
-// function drawInitialsIcon(canvasId, text) {
-//   var canvas = document.getElementById(canvasId);
-//   var ctx = canvas.getContext('2d');
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//   ctx.fillStyle = '#007bff'; // Background color
-//   ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-//   ctx.fillStyle = 'white'; // Text color
-//   ctx.font = 'bold 24px Arial';
-//   ctx.textAlign = 'center';
-//   ctx.textBaseline = 'middle';
-//   ctx.fillText(text, canvas.width / 2, canvas.height / 2);
-// }
-
-// function getInitials(name) {
-//   return name.split(' ').map(n => n[0]).join('').toUpperCase();
-// }
 
 document.addEventListener("turbo:load", () => {
   const pathInput = document.querySelector('input[name="path"]');
