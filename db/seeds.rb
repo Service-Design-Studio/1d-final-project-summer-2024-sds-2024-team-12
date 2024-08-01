ec3079c8baad93c36206029753a9a7fd0314c3dd
zizipurple <tanzihui823@gmail.com>
2024-08-01 20:54:06 +0800

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,8 @@ Contact.create(name: "Mom", phone_number: "12345678")
 Contact.create(name: "Nicole", phone_number: "87654321")
 Contact.create(name: "Tom", phone_number: "76342561")
 Contact.create(name: "John", phone_number: "33245576")
+
+Contact.create(name: "mom", phone_number: "12345678")
+Contact.create(name: "nicole", phone_number: "87654321")
+Contact.create(name: "tom", phone_number: "76342561")
+Contact.create(name: "john", phone_number: "33245576")
